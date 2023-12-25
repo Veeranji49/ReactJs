5ae5855e61c9acfb8b46ad3f00053725a4438766
J Veeranji <153510420+Veeranji49@users.noreply.github.com>
2023-12-25 19:10:20 +0530

    Add files via upload

---
+++ Styles1.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Styles1=()=>{
+    return(
+        <div>
+            <h1 style={{color:"red",background:"blue"}}>Welcome to ReactJs</h1>
+        </div>
+    );
+}
+export default Styles1;
---
+++ Styles2.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Styles2=()=>{
+    const Styles={
+        Veeru:{
+            color:"red",
+            backgroundColor:"blue"
+        }
+    };
+    return(
+        <div>
+            <h1 style={Styles.Veeru}>Welcome to ReactJs Library</h1>
+        </div>
+    );
+}
+export default Styles2;
