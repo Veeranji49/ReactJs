160031efed7c3f9b7a39516b55c3975e5fd5ecf7
J Veeranji <153510420+Veeranji49@users.noreply.github.com>
2024-01-02 11:55:44 +0530

    Add files via upload

---
+++ Lazy-Loading/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About=()=>{
+    return(
+        <div>
+            <h1 style={{color:"red"}}>About</h1>
+            <p>Cristiano Ronaldo scores far more goals with his head, not afraid to go in where it hurts. He manages to apply great power to his headers and undoubtly is always going to be more effective in the air than Messi. This is due to the fact that Ronaldo stands at 6'1" while Lionel Messi stands at 5'6 ½". Messi scored the memorable second goal against Manchester United, one of England's best teams, in the 2009 Champions league final with his head. But that was the exception to the rule. Most of "La Pulga Atomica’s" (The Atomic Flea) goals come from his left foot. Lionel Messi has become a lethal intruder in the goalkeeper's area. If someone were to tell you that they've seen him gently push in 40 goals with one-touch kicks inside the goalkeeper's area the past two years, they wouldn't be exaggerating. When the play needs collected finishing, Lionel is right there to lightly push it home every time. 
+
+Read More
+Heroes Of Sports : Personal Heroes And Villains In Sports
+1318 Words | 6 Pages
+Ricardo Kaka, renowned AC Milan/Real Madrid player and native Brazilian, he is one of the most openly religious players in soccer and has contributed much of his personal funds to founding churches and supporting charities. Furthermore, Ricardo Kaka demonstrates a level of humility not often seen in the sporting world especially at the level he has competed at and the awards that he has received. After receiving the Ballon D’Or, the highest individual award in soccer, in 2007 (the last player to do so since the Lionel Messi and Cristiano Ronaldo winning streak) he attributed the skills 
+
+Read More
+Landon Donovan Research Paper
+1099 Words | 5 Pages
+Landon Donovan was born on March 4, 1982 in Ontario, California. Donovan, his sister Tristan and older brother Josh, were raised primarily by their mother. Their father played semi-pro hockey. Donovan was a natural athlete. At the age of 5, he began playing competitive soccer. By the time he entered High School he was also known</p>
+        </div>
+    );
+}
+export default About;
---
+++ Lazy-Loading/Reactjss.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Reactjss=()=>{
+    return(
+        <div>
+            <h1 style={{color:"skyblue"}}>ReactJs</h1>
+        </div>
+    );
+}
+export default Reactjss;
